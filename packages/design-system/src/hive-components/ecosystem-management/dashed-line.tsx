88b6ee24607722927f8175cf896feb445e856ed6
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2025-11-24 17:43:47 +0100

    Bump components to 9.11.2 to get new HiveNavigation animation (#7309)

--- packages/design-system/src/hive-components/ecosystem-management/dashed-line.tsx
+++ packages/design-system/src/hive-components/ecosystem-management/dashed-line.tsx
@@ -5,22 +5,22 @@ export interface DashedLineProps extends React.SVGProps<SVGSVGElement> {
   short?: boolean;
 }
 
-export function DashedLine(props: DashedLineProps) {
+export function DashedLine({ short, className, ...rest }: DashedLineProps) {
   const commonProps: React.SVGProps<SVGSVGElement> = {
     preserveAspectRatio: 'none',
     stroke: 'currentColor',
     fill: 'none',
     vectorEffect: 'non-scaling-stroke',
-    ...props,
+    ...rest,
   };
 
-  if (props.short) {
+  if (short) {
     return (
       <svg
         {...commonProps}
         width="111"
         viewBox="0 0 111 114"
-        className={cn('h-full overflow-visible', css['animate-dash-reverse'], props.className)}
+        className={cn('h-full overflow-visible', css['animate-dash-reverse'], className)}
       >
         <path
           d="M0 112.5H31.3352C44.59 112.5 55.3351 101.755 55.3352 88.5001L55.3355 25.4999C55.3356 12.2451 66.0807 1.50001 79.3355 1.5L111 1.5"
@@ -36,7 +36,7 @@ export function DashedLine(props: DashedLineProps) {
       {...commonProps}
       width="107"
       viewBox="0 0 107 326"
-      className={cn('h-full overflow-visible', css['animate-dash'], props.className)}
+      className={cn('h-full overflow-visible', css['animate-dash'], className)}
     >
       <path
         d="M 150 0 H 77.659 c -13.255 0 -24 10.745 -24 24 V 303.5 c 0 13.255 -10.746 24 -24 24 H 0"
