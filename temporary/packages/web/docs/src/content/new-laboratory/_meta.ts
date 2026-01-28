b26caead857458398e450ddbab7035cc4da4e5cf
Michael Skorokhodov <michael@the-guild.dev>
2026-01-28 14:39:53 +0100

    Enhancement: New lab docs (#7560)

---
+++ temporary/packages/web/docs/src/content/new-laboratory/_meta.ts
@@ -0,0 +1,9 @@
+export default {
+  index: 'Overview',
+  operations: 'Operations',
+  'schema-support': 'Schema Support',
+  collections: 'Collections',
+  preflight: 'Preflight Scripts',
+  'environment-variables': 'Environment Variables',
+  history: 'History',
+};
