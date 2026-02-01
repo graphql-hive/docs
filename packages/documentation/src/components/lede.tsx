45fdd0c2f40e0e8e655832ce3445890ea4b6bfe1
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2026-02-01 11:44:40 +0100

    Port case study components, add #components subpath imports

---
+++ packages/documentation/src/components/lede.tsx
@@ -0,0 +1,9 @@
+import { cn } from "@hive/design-system/cn";
+
+export interface LedeProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function Lede(props: LedeProps) {
+  return (
+    <div {...props} className={cn("sm:*:text-xl/8 md:*:text-2xl/8", props.className)} />
+  );
+}
