3f02bcac88b57bd8918879370ffb0117fb97f1e3
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2026-02-24 19:12:43 +0100

    fix navbar width and mx-auto

--- packages/design-system/src/hive-navigation/index.tsx
+++ packages/design-system/src/hive-navigation/index.tsx
@@ -154,6 +154,7 @@ export function HiveNavigation({
   return (
     <div
       className="sticky top-0 z-20 border-b border-beige-400/(--border-opacity) bg-[rgb(var(--nextra-bg))] p-4 md:px-6 text-green-1000 transition-[border-color] duration-500 md:mb-1.75 md:mt-2 dark:border-neutral-700/(--border-opacity) dark:text-neutral-200 row-1 [grid-area:nav]"
+      id="hive-navigation"
       ref={containerRef}
       style={{ "--border-opacity": "0%" } as React.CSSProperties}
     >
@@ -179,7 +180,11 @@ export function HiveNavigation({
 
       {/* desktop menu */}
       <NavigationMenu
-        className={cn("mx-auto hidden md:flex", WIDTH_STYLE, className)}
+        className={cn(
+          "mx-auto w-360 max-w-full hidden md:flex",
+          WIDTH_STYLE,
+          className,
+        )}
         delayDuration={0}
       >
         {resolvedLogo}
