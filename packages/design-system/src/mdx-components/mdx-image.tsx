f9d0564ca4b9d3c223bfd136be22a3341cd2ce13
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2026-03-06 12:31:45 +0100

    Add MdxImage component with click-to-zoom
    New image component using react-medium-image-zoom, responsive sizes,
    and rounded styling. Replaces fumadocs default img in MDX.

---
+++ packages/design-system/src/mdx-components/mdx-image.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import type { ImgHTMLAttributes } from "react";
+
+import Zoom from "react-medium-image-zoom";
+
+import { cn } from "../cn";
+import "./image-zoom.css";
+
+export function MdxImage({
+  className,
+  ...props
+}: ImgHTMLAttributes<HTMLImageElement>) {
+  return (
+    <Zoom
+      wrapElement="span"
+      zoomImg={{ sizes: undefined, src: props.src }}
+      zoomMargin={20}
+    >
+      <img
+        className={cn("rounded-lg", className)}
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 70vw, 900px"
+        {...props}
+      />
+    </Zoom>
+  );
+}
