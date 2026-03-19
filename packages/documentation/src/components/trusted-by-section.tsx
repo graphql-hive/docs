27033699c0ebdec56c4765a78ae8d130277de57b
Piotr Monwid-Olechnowicz <hasparus@gmail.com>
2026-03-19 22:03:39 +0100

    Add hover background to Hemnet link

--- packages/documentation/src/components/trusted-by-section.tsx
+++ packages/documentation/src/components/trusted-by-section.tsx
@@ -21,10 +21,13 @@ export function TrustedBySection(props: React.HTMLAttributes<HTMLElement>) {
         <LinktreeLogo height={22} title="Linktree" />
         <KarrotLogo height={28} title="Karrot" />
         <AligentLogo height={32} title="Aligent" />
-        <SoundYXZLogo height={32} title="SoundXYZ" />
-        <Anchor href="/case-studies/hemnet">
+        <Anchor
+          className="hover:bg-beige-100 dark:bg-neutral-800 px-3 py-2 -my-2 -mx-3 rounded-lg"
+          href="/case-studies/hemnet"
+        >
           <HemnetLogo height={32} title="Hemnet" />
         </Anchor>
+        <SoundYXZLogo height={32} title="SoundXYZ" />
       </div>
     </div>
   );
